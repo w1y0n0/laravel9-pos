9f94a4cbac14acdabc6381219a433329f49290e4
w1y0n0 <wiyono30@gmail.com>
2023-07-13 22:55:28 +0700

    database migration,seed,model,timezone

---
+++ database/seeders/UserData.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserData extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            [
+                'name' => 'Administrator',
+                'username' => 'admin',
+                'password' => bcrypt('12345'),
+                'level' => 1,
+                'email' => 'admin@labsiproject.com'
+            ],
+            [
+                'name' => 'Kasir',
+                'username' => 'kasir',
+                'password' => bcrypt('12345'),
+                'level' => 1,
+                'email' => 'kasir@labsiproject.com'
+            ],
+            [
+                'name' => 'Pimpinan',
+                'username' => 'pimpinan',
+                'password' => bcrypt('12345'),
+                'level' => 1,
+                'email' => 'pimpinan@labsiproject.com'
+            ],
+        ];
+
+        foreach ($user as $key => $value) {
+            User::create($value);
+        }
+    }
+}
